f4abfe3d7ea78cb35ced7c70b24bafb57d55e609
Huseyn Baymammadli <huseynnb@code.edu.az>
2023-03-05 17:28:59 +0400

    Add project files.

---
+++ Inheritance1/ProDocumentProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance1
+{
+    class ProDocumentProgram : DocumentProgram
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("DocumentEdited");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document Saved in doc format,");
+        }
+    }
+}
---
+++ Inheritance1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Inheritance1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        check: Console.WriteLine("Enter");
+            var user = Console.ReadLine();
+
+            if (user == "basic")
+            {
+                var basic = new DocumentProgram();
+                basic.level = "Basic";
+                basic.OpenDocument();
+                basic.EditDocument();
+                basic.SaveDocument();
+            }
+            else if (user == "Pro")
+            {
+                var pro = new ProDocumentProgram();
+                pro.level = "pro";
+                pro.OpenDocument();
+                pro.EditDocument();
+                pro.SaveDocument();
+
+            }
+            else if (user == "Expert")
+            {
+                var expert = new ExpertDocumentProgram();
+                expert.level = "expert";
+                expert.OpenDocument();
+                expert.EditDocument();
+                expert.SaveDocument();
+            }
+            else
+            {
+                Console.WriteLine("not exsist");
+                goto check;
+            }
+        }
+    }
+}
